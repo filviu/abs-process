807d756a09cf88390b0f5a16cc7eff9b12e28568
filviu <2470335+filviu@users.noreply.github.com>
2024-11-03 21:57:22 +0000

    rework to add all collections, not just first

--- abs-process.py
+++ abs-process.py
@@ -3,7 +3,7 @@ import argparse
 import requests
 import re
 import math
-
+import sys
 
 # Define the base URL of your AudiobookShelf instance and the API key
 
@@ -12,7 +12,12 @@ mismatched_asins = []
 
 # todo create collections via API if not present
 
-def read_config(config_file):
+def add_missing_to_list(in_obj, in_list):
+    if in_obj not in in_list:
+        in_list.append(in_obj)
+    #return in_list
+
+def get_ini_config(config_file):
     global g_base_url, g_api_key, g_library_name, headers
     abs_config = {}
     with open(config_file, 'r') as f:
@@ -29,20 +34,39 @@ def read_config(config_file):
         'Authorization': f'Bearer {g_api_key}'
     }
 
-def get_library_id(g_library_name):
+def init_library_id(g_library_name):
+    global g_library_id
+
     print(f'Get library id for {g_library_name}... ', end='')
     response = requests.get(f'{g_base_url}/libraries', headers=headers)
     response.raise_for_status()
     for library in response.json()["libraries"]:
         if library["name"] == g_library_name:
             print(f'{library["id"]}')
-            return library["id"]
-    return None  # not found
+            g_library_id=library["id"]
+            return True
+    return False
 
-def get_all_items(library_id, limit):
+def init_collections_json():
+    global g_collections_json
+
+    print(f'Get all collections... ', end='')
+    response = requests.get(f'{g_base_url}/collections/', headers=headers)
+    response.raise_for_status()
+    g_collections_json =  response.json()
+    # trim for performance - not sure it helps
+    for collection in g_collections_json['collections']:
+        for book in collection['books']:
+            if 'media' in book:
+                del book['media']
+            if 'libraryFiles' in book:
+                del book['libraryFiles']
+    print(f'[DONE]')
+
+def get_all_items(limit):
     # Fetch all items from the library
     print(f'Get all items... ',end='')
-    response = requests.get(f'{g_base_url}/libraries/{library_id}/items?sort=addedAt&desc=1&limit={limit}', headers=headers)
+    response = requests.get(f'{g_base_url}/libraries/{g_library_id}/items?sort=addedAt&desc=1&limit={limit}', headers=headers)
     response.raise_for_status()
     print(f'[DONE]')
     return response.json()['results']
@@ -59,62 +83,29 @@ def get_all_batch(all_items_json):
     except:
         print(f'Error getting bulk object')
 
-def get_collections():
-    print(f'Get all collections... ', end='')
-    response = requests.get(f'{g_base_url}/collections/', headers=headers)
-    response.raise_for_status()
-    json_data =  response.json()
-    # trim for performance - not sure it helps
-    for collection in json_data['collections']:
-        for book in collection['books']:
-            if 'media' in book:
-                del book['media']
-            if 'libraryFiles' in book:
-                del book['libraryFiles']
-    print(f'[DONE]')
-    return json_data
-
-def add_to_collection(item_id, collection_id, collection_name):
-    data = {'id': item_id}
-    try:
-        response = requests.post(f'{g_base_url}/collections/{collection_id}/book', headers=headers, json=data)
-        response.raise_for_status()
-        return response.json()
-    except:
-        print(f'(Error adding {item_id} to {collection_name} collection')
-
-def update_tags(id, tags):
-    data = {'tags': tags}
-    try:
-        response = requests.patch(f'{g_base_url}/items/{id}/media', headers=headers, json=data)
-        response.raise_for_status()
-        return response.json()
-    except:
-        print(f'Error adding {tags} to {id} item')
-
-def get_item(id):
-    response = requests.get(f'{g_base_url}/items/{id}', headers=headers)
+def get_item(item_id):
+    response = requests.get(f'{g_base_url}/items/{item_id}', headers=headers)
     response.raise_for_status()
     return response.json()
 
-def get_item_files(id):
-    response = requests.get(f'{g_base_url}/items/{id}', headers=headers)
+def get_item_files(item_id):
+    response = requests.get(f'{g_base_url}/items/{item_id}', headers=headers)
     response.raise_for_status()
     return response.json()['media']['audioFiles']
 
-def book_id_in_collection(book_id, collection_name, collections_json):
-    for collection in collections_json["collections"]:
+def book_id_in_collection(book_id, collection_name):
+    for collection in g_collections_json["collections"]:
         if collection["name"] == collection_name:
             return any(book["id"] == book_id for book in collection["books"])
     return False
 
-def getset_collection_id_by_name(library_id, collection_name, collections_json):
-    for collection in collections_json['collections']:
+def getset_collection_id_by_name(collection_name):
+    for collection in g_collections_json['collections']:
         if collection['name'] == collection_name:
             return collection['id']
     # if we didn't return here it means the collection doesn't exist
     data = {
-        'libraryId': LIB_ID,
+        'libraryId': g_library_id,
         'name': collection_name
         }
     try:
@@ -125,11 +116,46 @@ def getset_collection_id_by_name(library_id, collection_name, collections_json):
         print(f'(Failed to create new collection {collection_name}')
     return None
 
-def main():
+def getset_cached_collection_id(collection_name):
+    global collection_id_cache
+    if collection_name in collection_id_cache:
+        collection_id = collection_id_cache[collection_name]  # Use cached result
+    else:
+        collection_id = getset_collection_id_by_name(collection_name)
+        collection_id_cache[collection_name] = collection_id  # Cache the result
+    return collection_id
 
-    read_config("abs.ini")
-    
-    library_id=get_library_id(g_library_name)
+def add_to_collection(item_id, item_title, collection_name):
+
+    if not book_id_in_collection(item_id, collection_name):
+        print(f'\033[92m[adding to {collection_name}]\033[00m - {item_title} ({item_id})')
+        collection_id=getset_cached_collection_id(collection_name)
+
+        try:
+            data = {'id': item_id}
+            response = requests.post(f'{g_base_url}/collections/{collection_id}/book', headers=headers, json=data)
+            response.raise_for_status()
+            return response.json()
+        except:
+            print(f'(Error adding {item_id} to {collection_name} collection')
+    else:
+        print(f'\033[93m[exists in {collection_name}]\033[00m - {item_title} ({item_id})')
+
+def set_item_tags(item_id, tags):
+    data = {'tags': tags}
+    try:
+        response = requests.patch(f'{g_base_url}/items/{item_id}/media', headers=headers, json=data)
+        response.raise_for_status()
+        return response.json()
+    except:
+        print(f'Error adding {tags} to {item_id} item')
+
+def main():
+    get_ini_config("abs.ini")
+    if not init_library_id(g_library_name):
+        sys.exit("Library not found.")
+
+    init_collections_json()
 
     # Parse command-line arguments
     parser = argparse.ArgumentParser(description='Fetch items from a library.')
@@ -141,9 +167,7 @@ def main():
     else:
         item_limit = 0
 
-    collections = get_collections()
-
-    batch_items = get_all_batch(get_all_items(library_id, item_limit))
+    batch_items = get_all_batch(get_all_items(item_limit))
 
     #with open('get_all_batch.json', 'w') as f:
     #    json.dump(batch_items, f)
@@ -155,54 +179,47 @@ def main():
     for item in batch_items:
         item_id = item['id']
         item_title = item['media']['metadata']['title']
-        item_tags = item['media']['tags']
+        item_tags = list(item['media']['tags'])
         item_files = item['media']['audioFiles']
         item_asin = item['media']['metadata']['asin']
 
-        print(f'\033[36m{i:0{total_digits}} of {total}\033[40m', end=' ')
+        print(f'\033[36m{i:0{total_digits}} of {total}\033[40m')
 
         for item_file in item_files:
             filename = item_file['metadata']['filename']
             filepath = item_file['metadata']['path']
             if filename.endswith(".m4b"):
                 if '.com.' in filename:
-                    collection_name = 'audible.com'
-                elif '.co_uk.' in filename:
-                    collection_name = 'audible.co.uk'
-                elif 'LATW/' in filepath:
-                    collection_name = 'LATW'
-                elif 'The Great Courses/' in filepath:
-                    collection_name = 'The Great Courses'
-                elif re.search(r'\[\w+\]', filename) and '.com.' not in filename and '.co_uk.' not in filename:
-                    collection_name = 'audible_legacy'
-                else:
-                    continue
+                    extra_name = 'audible.com'
+                    add_to_collection(item_id, item_title, extra_name)
+                    add_missing_to_list(extra_name, item_tags)
+                if '.co_uk.' in filename:
+                    extra_name = 'audible.co.uk'
+                    add_to_collection(item_id, item_title, extra_name)
+                    add_missing_to_list(extra_name, item_tags)
+                if re.search(r'\[\w+\]', filename) and '.com.' not in filename and '.co_uk.' not in filename:
+                    extra_name = 'audible_legacy'
+                    add_to_collection(item_id, item_title, extra_name)
+                    add_missing_to_list(extra_name, item_tags)
+                if 'LATW/' in filepath:
+                    extra_name = 'LATW'
+                    add_to_collection(item_id, item_title, extra_name)
+                    add_missing_to_list(extra_name, item_tags)
+                if 'The Great Courses/' in filepath:
+                    extra_name = 'The Great Courses'
+                    add_to_collection(item_id, item_title, extra_name)
+                    add_missing_to_list(extra_name, item_tags)
 
-                if not book_id_in_collection(item_id, collection_name, collections):
-                    print(f'\033[92m[adding to {collection_name}]\033[00m - {item_id} ({item_title})',end="")
+                if item_tags != item['media']['tags']:
+                    print(f'\033[96mAdding tags: {[tag for tag in item_tags if tag not in item['media']['tags']]}\033[00m')
+                    set_item_tags(item_id, item_tags)
 
-                    if collection_name in collection_id_cache:
-                        collection_id = collection_id_cache[collection_name]  # Use cached result
-                    else:
-                        # Call the API and store the result in the cache
-                        collection_id = getset_collection_id_by_name(library_id, collection_name, collections)
-                        collection_id_cache[collection_name] = collection_id  # Cache the result
-                    add_to_collection(item_id, collection_id, collection_name)
-                else:
-                    print(f'\033[93m[exists in {collection_name}]\033[00m - {item_id} ({item_title})',end="")
-
-                if collection_name not in item_tags:
-                        print(f' - \033[96m tagging {collection_name}\033[00m',end="")
-                        item_tags.append(collection_name)
-                        update_tags(item_id, item_tags)
-
-                file_asin_matches = [m for m in re.findall(r"\[(.*?)\]", filename) if len(m) >= 4]
+                file_asin_matches = [m for m in re.findall(r"\[(.*?)\]", filename) if len(m) == 10]
                 if file_asin_matches:
                     file_asin = file_asin_matches[-1]
                     if file_asin!= item_asin:
                         mismatched_asins.append(item_title)
 
-        print(f'')
         i += 1
 
     print(f'\n\033[31m{len(mismatched_asins)} ASIN mismatches!\033[00m\n')
